3f7cc7a8b56342a56960e2d50d13296aa043ffdd
Ketan Padegaonkar <ketanpadegaonkar@gmail.com>
2014-12-02 15:57:32 +0800

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -1,12 +1,4 @@
 #!/usr/bin/env bash
 echo "Provisioning"
 
-sed "s/us.archive.ubuntu.com/mirror.nus.edu.sg/" < /etc/apt/sources.list > s1.list
-sed "s/security.ubuntu.com/mirror.nus.edu.sg/" < s1.list > sources.list
-mv sources.list /etc/apt
-
 apt-get update
-apt-get install -y python-software-properties
-add-apt-repository ppa:git-core/ppa
-apt-get update
-apt-get install -y git
